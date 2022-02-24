1af18665db164a69481579ef22d6b6156368d62d
Trần Duy Long <tdl@netcompany.com>
2022-02-24 13:30:26 +0700

    add extension

---
+++ client/src/pages/landing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='East India' className='logo'/>
+      </nav>
+      <div className='container page'>
+        
+      </div>
+    </main>
+  )
+}
+
+export default landing
