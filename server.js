bbe98d530f68745f38bf06f50b1599e6cfac9b70
Thanh Nguyen <jamesnguyen1st@gmail.com>
2022-02-24 11:51:11 +0700

    set up server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Server running");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is on port ${port}`);
+});
